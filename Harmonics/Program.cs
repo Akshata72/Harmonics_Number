ed265c4c9c5a707f4f9ad17b53d2deb319c61ed1
Akshata72 <sableakshata880@gmail.com>
2022-03-12 21:37:31 +0530

    Added:Harmonics_Number

---
+++ Harmonics/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Harmonics
+{
+
+    class Harmonics
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Input the Number of terms:");
+            int Num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= Num; i++)
+            {
+                Console.Write("1/{0} + ", i);
+                
+
+            }
+
+
+        
+        }
+    }
+
+
+
+}
+
+    
